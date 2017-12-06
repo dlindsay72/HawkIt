73677b0b234f098b8e0a9210086447c0234ff5b1
Dan Lindsay <dlindsay72@hotmail.com>
2017-12-05 21:23:53 -0500

    installed Firebase cocoapods, created FIRDatabaseService

---
+++ HawkIt/Services/FIRDatabaseService.swift
@@ -0,0 +1,37 @@
+//
+//  FIRDatabaseService.swift
+//  HawkIt
+//
+//  Created by Dan Lindsay on 2017-12-05.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+enum FIRDatabaseReference: String {
+    case products = "products"
+    
+}
+
+class FIRDatabaseService {
+    
+    private init() {}
+    
+    static let shared = FIRDatabaseService()
+    
+    func reference(_ databaseReference: FIRDatabaseReference) -> DatabaseReference {
+        
+        return Database.database().reference().child(databaseReference.rawValue)
+    }
+    
+    func observe(_ databaseReference: FIRDatabaseReference, completion: @escaping (DataSnapshot) -> Void) {
+        reference(databaseReference).observe(.value) { (snapshot) in
+            completion(snapshot)
+        }
+    }
+    
+    func post(parameters: [String: Any], to databaseReference: FIRDatabaseReference) {
+        reference(databaseReference).childByAutoId().setValue(parameters)
+    }
+}
---
+++ Pods/Target Support Files/Pods-HawkIt/Pods-HawkIt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HawkIt : NSObject
+@end
+@implementation PodsDummy_Pods_HawkIt
+@end
